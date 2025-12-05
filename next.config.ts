64590611641a383ca5f98278f705b8ee0ddd137d
Stephen Swaringin <stephen.swaringin@gmail.com>
2025-12-05 08:29:09 -0600

    starting project using custom node module (had to adjust config to make turbopack see that module locally)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: path.join(__dirname, ".."), // needed for using locally developed modules
+  },
 };
 
 export default nextConfig;
