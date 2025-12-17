237d9c59bbf1e2b7786fde0e4bbfe6aa69ccc3b2
Stephen Swaringin <stephen.swaringin@gmail.com>
2025-12-17 10:46:19 -0600

    testing next.js routes

---
+++ src/app/api/route.ts
@@ -0,0 +1,16 @@
+// export async function GET(request: Request) {
+//   // return Response.json({ message: "Hello Next.js world!" });
+// }
+
+// export async function HEAD(request: Request) {}
+
+// export async function POST(request: Request) {}
+
+// export async function PUT(request: Request) {}
+
+// export async function DELETE(request: Request) {}
+
+// export async function PATCH(request: Request) {}
+
+// If `OPTIONS` is not defined, Next.js will automatically implement `OPTIONS` and set the appropriate Response `Allow` header depending on the other methods defined in the Route Handler.
+// export async function OPTIONS(request: Request) {}
